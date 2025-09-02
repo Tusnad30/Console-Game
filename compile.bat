03e00c98ab332c7c4d6a8381fbf69fbced7b59de
Tusnad30 <89738410+Tusnad30@users.noreply.github.com>
2025-09-02 13:53:17 +0200

    Updated compile.bat with new compilation and linking arguments.

--- compile.bat
+++ compile.bat
@@ -2,6 +2,5 @@
 
 call vcvars32.bat
 echo:
-cl /O1 main.c /link /NOLOGO /SUBSYSTEM:CONSOLE /ENTRY:main /ALIGN:16 /NODEFAULTLIB ucrt.lib vcruntime.lib msvcrt.lib user32.lib
-
-start main.exe
+cl /O1 /Os /GS- /W4 main.c /link /NOLOGO /SUBSYSTEM:CONSOLE /ENTRY:main /ALIGN:16 /NODEFAULTLIB ucrt.lib vcruntime.lib msvcrt.lib user32.lib kernel32.lib
+pause
